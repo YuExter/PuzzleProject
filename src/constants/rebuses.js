d728cff87619dbed891877f23912b762050b1c0d
nazarukroman <nazarukroman.dev@gmail.com>
2021-12-09 20:38:51 +0300

    Add logic

--- src/constants/rebuses.js
+++ src/constants/rebuses.js
@@ -4,10 +4,10 @@ import puzzle3 from '../../assets/puzzle3.png';
 import puzzle4 from '../../assets/puzzle4.png';
 import puzzle5 from '../../assets/puzzle5.png';
 
-export const rebuses = [{ image: puzzle1, answer: 'блокчейн' }, {
-  image: puzzle2,
-  answer: 'джуниор девелопер',
-}, { image: puzzle3, answer: 'туториал' }, { image: puzzle4, answer: 'хакатон' }, {
-  image: puzzle5,
-  answer: 'фронтенд',
-}];
+export const rebuses = [
+  { image: puzzle1, answer: 'блокчейн' },
+  { image: puzzle2, answer: 'джуниор девелопер' },
+  { image: puzzle3, answer: 'туториал' },
+  { image: puzzle4, answer: 'хакатон' },
+  { image: puzzle5, answer: 'фронтенд' },
+];
