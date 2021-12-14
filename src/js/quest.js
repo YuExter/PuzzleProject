ce48cc9d7a7e692d93dc18ce33fb71431aee73db
nazarukroman <nazarukroman.dev@gmail.com>
2021-12-14 11:05:00 +0300

    update

--- src/js/quest.js
+++ src/js/quest.js
@@ -87,7 +87,7 @@ export class Quest {
       event.preventDefault();
 
       const formData = new FormData(event.target);
-      const variantValue = formData.fd.get('variant');
+      const variantValue = formData.get('variant');
 
       if (!this.variantInput) {
         return;
